6ced2039306572ae445729f32a9b23972b2cb369
z1205595931 <32691797+z1205595931@users.noreply.github.com>
2026-04-15 18:26:55 +0800

    Update MyApp.kt

--- app/src/main/java/com/example/myproxy/MyApp.kt
+++ app/src/main/java/com/example/myproxy/MyApp.kt
@@ -1,27 +1,22 @@
 package com.example.myproxy
 
 import android.app.Application
-import com.aliyun.ams.httpdns.HTTPDNS
-import com.aliyun.ams.httpdns.HTTPDNSResolver
+import com.alibaba.sdk.android.httpdns.HttpDns
+import com.alibaba.sdk.android.httpdns.InitConfig
 
 class MyApp : Application() {
-
-    companion object {
-      // 从阿里云控制台获取
+    override fun onCreate() {
+        super.onCreate()
         private const val ACCOUNT_ID = "118094"
         private const val ACCESS_KEY_ID = "6e4e74a8ff8685da1138eab88c6032c3"
         private const val ACCESS_KEY_SECRET = "b54db43230711ced72b2334a44612b8f"
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
+        
         // 初始化 HTTPDNS SDK
-        HTTPDNS.init(this, ACCOUNT_ID, ACCESS_KEY_ID, ACCESS_KEY_SECRET)
-        // 预加载域名
-        HTTPDNS.getInstance().preLoadDomains(
-            HTTPDNS.QTYPE_IPV4,
-            arrayOf("v2.api.juliangip.com")
+        HttpDns.getService(applicationContext, accountID)
+        HttpDns.init(applicationContext, InitConfig.Builder()
+            .setContext(applicationContext)
+            .setSecretKey(secretKey)
+            .build()
         )
     }
 }
