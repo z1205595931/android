0d86924d0ebde090b2451f874ab1beea56d3cc66
z1205595931 <32691797+z1205595931@users.noreply.github.com>
2026-04-15 18:19:11 +0800

    Update MyApp.kt

--- app/src/main/java/com/example/myproxy/MyApp.kt
+++ app/src/main/java/com/example/myproxy/MyApp.kt
@@ -1,12 +1,13 @@
 package com.example.myproxy
 
 import android.app.Application
-import com.alibaba.sdk.android.httpdns.DNSResolver
+import com.aliyun.ams.httpdns.HTTPDNS
+import com.aliyun.ams.httpdns.HTTPDNSResolver
 
 class MyApp : Application() {
 
     companion object {
-        // 从阿里云控制台获取
+      // 从阿里云控制台获取
         private const val ACCOUNT_ID = "118094"
         private const val ACCESS_KEY_ID = "6e4e74a8ff8685da1138eab88c6032c3"
         private const val ACCESS_KEY_SECRET = "b54db43230711ced72b2334a44612b8f"
@@ -15,13 +16,14 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // 初始化 HTTPDNS SDK，确保尽早执行
-        DNSResolver.Init(this, ACCOUNT_ID, ACCESS_KEY_ID, ACCESS_KEY_SECRET)
-
-        // （可选）预加载域名，可加速首次解析。此处以巨量IP API域名为例。
-        DNSResolver.getInstance().preLoadDomains(
-            DNSResolver.QTYPE_IPV4,
+        // 初始化 HTTPDNS SDK
+        HTTPDNS.init(this, ACCOUNT_ID, ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+        // 预加载域名
+        HTTPDNS.getInstance().preLoadDomains(
+            HTTPDNS.QTYPE_IPV4,
             arrayOf("v2.api.juliangip.com")
         )
     }
 }
+        
+    
