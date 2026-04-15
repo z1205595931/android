05e3ca1b0d632257f1a01b66da4795317f38a257
z1205595931 <32691797+z1205595931@users.noreply.github.com>
2026-04-15 20:21:55 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,10 @@ android {
 }
 
 dependencies {
-      implementation("androidx.core:core-ktx:1.12.0")
+     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")      // 使用最新稳定版
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
 
